15a4ca996ebc729884431d48c72c2f64eefcd007
Felix Steiner <felix_steiner@bluewin.ch>
2025-05-14 23:14:52 +0200

    fix(exports): fix import (add file extensions)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import convertVideo from './convertVideo';
-import type { NormalizedParameters } from './types/NormalizedParameters';
+import convertVideo from './convertVideo.js';
+import type { NormalizedParameters } from './types/NormalizedParameters.js';
 
 export { convertVideo, type NormalizedParameters };
